3a954da1a74f153e59861c2d36f0678e7b735d3c
Moritz Küttel <moritz_kuettel+composer_ci4dbtransl@fastmail.com>
2024-03-12 05:08:18 +0100

    add more arguments for transaction service declaration

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -5,24 +5,23 @@ declare(strict_types=1);
 
 namespace MKU\Services\Config;
 
+use CodeIgniter\Database\BaseConnection;
 use MKU\Services\TransactionService;
 use CodeIgniter\Config\BaseService;
 use MKU\Services\Config\Transaction as TransactionConfig;
 
-class Services extends BaseService
-{
+class Services extends BaseService {
     /**
      * The TransactionService class
      */
-    public static function transaction(bool $getShared = true): TransactionService
-    {
+    public static function transaction(TransactionConfig $config = null, BaseConnection $db = null, bool $getShared = true): TransactionService {
         if ($getShared) {
-            return self::getSharedInstance('transaction');
+            return self::getSharedInstance('transaction', $config, $db);
         }
 
         /** @var TransactionConfig $config */
-        $config = config('transaction');
+        $config = $config ?? config('Transaction');
 
-        return new TransactionService($config, db_connect());
+        return new TransactionService($config, $db ?? db_connect());
     }
 }
