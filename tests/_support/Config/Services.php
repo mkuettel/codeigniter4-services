515cbfe3c993dbd257e240f7cda1d7c8bab85ee7
Moritz Küttel <moritz_kuettel+composer_ci4dbtransl@fastmail.com>
2024-03-12 05:46:39 +0100

    add example service class using multiple models, add some tests
    this will now check more code paths including stuff stuch as Storable

---
+++ tests/_support/Config/Services.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Tests\Support\Config;
+
+use CodeIgniter\Config\BaseService;
+use MKU\Services\TransactionService;
+use Tests\Support\Models\PageContentModel;
+use Tests\Support\Models\PageModel;
+use Tests\Support\Services\PageService;
+
+class Services extends BaseService {
+    /**
+     * The TransactionService class
+     */
+    public static function pages(TransactionService $transactions = null, PageModel $page_model = null, PageContentModel $page_content_model = null, bool $getShared = true): PageService {
+        if ($getShared) {
+            return self::getSharedInstance('pages', $transactions, $page_model, $page_content_model, $db);
+        }
+
+        return new PageService(
+            $transactions ?? \MKU\Services\Config\Services::transaction(),
+            $page_model ?? model(PageModel::class),
+            $page_content_model ?? model(PageContentModel::class),
+        );
+    }
+}
---
+++ tests/_support/Services/PageService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Support\Services;
+
+use CodeIgniter\Database\ResultInterface;
+use MKU\Services\ServiceException;
+use MKU\Services\Service;
+use MKU\Services\TransactionService;
+use Prewk\Result;
+use Tests\Support\Entities\Page;
+use Tests\Support\Models\PageContentModel;
+use Tests\Support\Models\PageModel;
+
+final class PageService implements Service {
+    private TransactionService $transaction;
+    private PageModel $pages;
+    private PageContentModel $contents;
+
+    public function __construct(TransactionService $transaction, PageModel $pages, PageContentModel $contents) {
+        $this->transaction = $transaction;
+        $this->pages = $pages;
+        $this->contents = $contents;
+    }
+
+    public function save(Page $page): Result {
+        return $this->transaction->transact(function () use (&$page) {
+            if (!$this->pages->validate($page)) {
+                return err($this->pages->errors());
+            }
+
+            foreach($page->contents ?? [] as $page_content) {
+                if(!$this->contents->validate($page_content)) {
+                    return err($this->contents->errors());
+                }
+            }
+
+            if (!$this->pages->store($page)) {
+                throw new ServiceException("Couldn't store page: " . json_encode($this->pages->errors()));
+            }
+
+            foreach($page->contents ?? [] as $page_content) {
+                $page_content->page_id = $page->id;
+                if(!$this->contents->store($page_content)) {
+                    throw new ServiceException("Couldn't store page content: " . json_encode($this->contents->errors()));
+                }
+            }
+
+            return ok($page);
+        });
+    }
+
+    public function get(int $id): ?Page {
+        $result = $this->pages->find($id);
+        if (is_object($result) && (int)$result->id === $id) {
+            return $result;
+        }
+
+        return null;
+    }
+
+    public function search(array $filters = []): ResultInterface {
+        $q = $this->pages->with('website_page_contents')->builder()->select();
+        if(isset($filters['title'])) {
+            $q = $q->whereIn(
+                'id',
+                $this->contents->builder()->select('page_id')->like('title', $filters['title'])
+            );
+        }
+        // TODO: implement some more filters
+
+        if(isset($filters['limit'])) {
+            $q = $q->limit($filters['limit']);
+        }
+        return $q->get();
+    }
+
+    public function delete(int $id): false|Page {
+        $page =  $this->get($id);
+        if($page === null) {
+            return false;
+        }
+
+        $result = $this->pages->delete($id);
+        if(!$result) {
+            return false;
+        }
+
+        return $page;
+    }
+}
