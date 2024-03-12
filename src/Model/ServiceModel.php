4c375ef2a04d2e1a03ab3d39c83ebd936a727806
Moritz Küttel <moritz_kuettel+composer_ci4dbtransl@fastmail.com>
2024-03-12 05:11:15 +0100

    let all service models support store() and relations

--- src/Model/ServiceModel.php
+++ src/Model/ServiceModel.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 namespace MKU\Services\Model;
 
 use CodeIgniter\Model;
+use Tatter\Relations\Traits\ModelTrait;
 
 abstract class ServiceModel extends Model {
     use Storable;
+    use ModelTrait;
 }
