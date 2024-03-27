5018c5e506a64037d69d1c8dc2cd915dd952f96e
Moritz Küttel <kuettel@hostmax.ch>
2024-03-27 17:27:30 +0100

    remove relations from test support models, entities and services

--- src/Model/ServiceModel.php
+++ src/Model/ServiceModel.php
@@ -3,9 +3,7 @@ declare(strict_types=1);
 namespace MKU\Services\Model;
 
 use CodeIgniter\Model;
-use Tatter\Relations\Traits\ModelTrait;
 
 abstract class ServiceModel extends Model {
     use Storable;
-    use ModelTrait;
 }
--- tests/_support/Models/PageModel.php
+++ tests/_support/Models/PageModel.php
@@ -4,12 +4,10 @@ namespace Tests\Support\Models;
 
 use MKU\Services\Model\ServiceModel;
 use MKU\Services\Model\Storable;
-use Tatter\Relations\Traits\ModelTrait;
 use Tests\Support\Entities\Page;
 
 final class PageModel extends ServiceModel {
     use Storable;
-    use ModelTrait;
 
 	protected $table = 'pages';
 
