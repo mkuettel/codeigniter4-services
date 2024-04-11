b3ea0827033305611a96a74192f512483485cecf
Moritz Küttel <moritz_kuettel+github_mkuettel@fastmail.com>
2024-04-11 09:11:56 +0200

    helpers: add transaction() helper function

---
+++ src/Helpers/transaction_helper.php
@@ -0,0 +1,7 @@
+<?php
+
+if(!function_exists('transaction')) {
+    function transaction(\Closure $func, ...$service_params) {
+        return service('transaction', ...$service_params)->transact(func);
+    }
+}
