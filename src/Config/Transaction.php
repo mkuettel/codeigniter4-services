5cbca7efdd0518ba3694c1607d52e42b4174c323
Moritz Küttel <moritz_kuettel+github_mkuettel@fastmail.com>
2024-04-11 09:03:22 +0200

    config: new configuration options in Transaction class

--- src/Config/Transaction.php
+++ src/Config/Transaction.php
@@ -4,4 +4,16 @@ namespace MKU\Services\Config;
 
 class Transaction {
 
+    public bool $testMode = false;
+
+    // Whether we should tell the CodeIgniter BaseConnection $db to use strict transaction
+    // this will result in better transaction isolation for some databases, if configured correctly,
+    // so that other connections cannot see data
+    // being modified during an ongoing transaction.
+    public bool $strictMode = false;
+
+    // Whether the TransactionService should throw TransactionException when an exception occurs during a
+    // transaction. If set to false, all exceptions during transaction will just be caught and logged as an error.
+    public bool $throwExceptions = true;
+
 }
