739bd48a53d14bb161f9b3855abeabcec1e1d4c8
Moritz Küttel <moritz_kuettel+github_mkuettel@fastmail.com>
2024-04-15 16:47:55 +0200

    model: clean up Storable Trait implementation

--- src/Model/Storable.php
+++ src/Model/Storable.php
@@ -3,27 +3,29 @@
 namespace MKU\Services\Model;
 
 use MKU\Services\Entity\ServiceEntity;
+use MKU\Services\ServiceException;
 
 trait Storable {
     /**
      * Store an entity in the database, either by updating or inserting it if required.
      *
      * @param ServiceEntity $entity
-     * @return bool whether the entity got stored in the database
+     * @return mixed false when the given entity could not be stored, else the primary key value of the entity is returned.
      */
-    public function store(ServiceEntity $entity): bool {
-        if($entity->getPrimaryKeyValue() !== null && !$entity->getPrimaryKeyName()) return true;
-        $saved = false;
+    public function store(ServiceEntity $entity): mixed {
+        if (empty($entity->getPrimaryKeyName())) throw new ServiceException("Can't store entity without a defined primary key, please implement getPrimaryKeyName() on the given Entity");
 
+        $saved = false;
         if ($this->shouldUpdate($entity)) {
-            $saved = !$entity->hasChanged() || $this->update($entity->getPrimaryKeyValue(), $entity);
+            if (!$entity->hasChanged()) return $entity->getPrimaryKeyValue();
+
+            $saved = $this->update($entity->getPrimaryKeyValue(), $entity);
+            $id = $entity->getPrimaryKeyValue();
         } else {
             $id = $this->insert($entity, true);
-            $saved = $id !== false && $entity->getPrimaryKeyName() !== null;
-            if ($saved) {
-                $entity->setPrimaryKeyValue($id);
-            }
+            $saved = $id !== false;
         }
-        return $saved;
+        $entity->setPrimaryKeyValue($id);
+        return $saved ? $id : false;
     }
 }
