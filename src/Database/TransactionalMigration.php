6364b74bedfe73d3e74384226c7b6ca7599f7afa
Moritz Küttel <moritz_kuettel+github_mkuettel@fastmail.com>
2024-04-15 16:58:37 +0200

    transaction: clean up the TransactionalMigration class

--- src/Database/TransactionalMigration.php
+++ src/Database/TransactionalMigration.php
@@ -2,22 +2,36 @@
 
 namespace MKU\Services\Database;
 
+use CodeIgniter\Database\Forge;
 use CodeIgniter\Database\Migration;
 use Config\Services;
+use MKU\Services\TransactionService;
 
+/**
+ * Runs the database migrations inside a transaction.
+ *
+ * @author Moritz Küttel
+ */
 abstract class TransactionalMigration extends Migration {
+
+    protected TransactionService $transaction;
+
+    public function __construct(Forge $forge = null) {
+        parent::__construct($forge);
+        $this->transaction = Services::transaction(null, $this->db);
+    }
+
     public function up(): void {
-        Services::transaction(null, $this->db)->transact(function () {
+        $this->transaction->transact(function () {
             $this->transactionalUp();
-        }, true);
+        });
     }
 
 
-    public function down(): void
-    {
-        Services::transaction(null, $this->db)->transact(function () {
+    public function down(): void {
+        $this->transaction->transact(function () {
             $this->transactionalDown();
-        }, true);
+        });
     }
 
     abstract function transactionalUp(): void;
