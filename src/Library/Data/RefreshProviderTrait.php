285633abdf4c2339ab9b28cfbf89986d001cfb16
Moritz Küttel <moritz_kuettel+github_mkuettel@fastmail.com>
2024-04-16 20:21:13 +0200

    persistence: fix RefreshProviderTrait

--- src/Library/Data/RefreshProviderTrait.php
+++ src/Library/Data/RefreshProviderTrait.php
@@ -5,12 +5,16 @@ namespace MKU\Services\Library\Data;
 use MKU\Services\Entity\ServiceEntity;
 
 /**
+ * Implements refresh() based upon get() and ServiceEntity methods for persistence and data provider services.
+ *
  * @author Moritz Küttel
  * @experimental
  */
 trait RefreshProviderTrait {
     public function refresh(ServiceEntity $entity): bool {
-        $refreshed = $this->get($entity->getPrimaryKeyValue();
+        if (!$entity->hasPrimaryKey()) { return false; }
+
+        $refreshed = $this->get($entity->getPrimaryKeyValue());
         if (!$refreshed) return false;
 
         $entity->setAttributes($refreshed->toArray());
