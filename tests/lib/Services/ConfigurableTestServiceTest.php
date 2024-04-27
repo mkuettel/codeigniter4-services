760fb40ad1e4c367920d2f38976700249595c53a
Moritz Küttel <moritz.kuettel+gitvbhome@fastmail.com>
2024-04-27 17:45:20 +0200

    phpunit: resolve namespace confusion with ConfigurableTest config

--- tests/lib/Services/ConfigurableTestServiceTest.php
+++ tests/lib/Services/ConfigurableTestServiceTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Lib\Services;
 
 use PHPUnit\Framework\TestCase;
-use Tests\Support\Config\ConfigurableTest as ConfigurableTestConfig;
+use \Config\ConfigurableTest as ConfigurableTestConfig;
 use Tests\Support\Services\ConfigurableTestService;
 
 class ConfigurableTestServiceTest extends TestCase {
